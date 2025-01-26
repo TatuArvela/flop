db6d99a5eb1d2aca068621b1e7c68e9fb473bf9c
Inari Tommiska <inari@jakojaannos.fi>
2025-01-26 16:22:46 +0200

    final koira v1

--- main_menu.gd
+++ main_menu.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://levels/alku.tscn")
 
 
 func _on_dont_button_pressed() -> void:
-	get_tree().quit() # Replace with function body.
+	get_tree().quit()
